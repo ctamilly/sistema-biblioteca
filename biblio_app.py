cd83d03563baac6d370c2d426eb0fa91c80deb5b
Guilherme Silva Tavares <136861352+aSHUA0@users.noreply.github.com>
2025-04-05 18:30:19 -0300

    Update menu sidebar

--- biblio_app.py
+++ biblio_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from livros import cadastrar_livro, listar_livros
 from usuarios import cadastrar_usuario, listar_usuarios
+from emprestimo import cadastrar_emprestimo, listar_emprestimos
+from devolucao import cadastrar_devolucao
 
 st.title("Sistema de Biblioteca")
 
-menu = st.sidebar.radio("Menu", ["Cadastrar Livro", "Listar Livros", "Cadastrar Usuário", "Listar Usuários"])
+menu = st.sidebar.radio("Menu", ["Cadastrar Livro", "Listar Livros", "Cadastrar Usuário", "Listar Usuários", "Realizar Emprestimos", "Mostrar Emprestimos", "Devolucoes"])
 
 if menu == "Cadastrar Livro":
     cadastrar_livro()
@@ -14,3 +16,9 @@ elif menu == "Cadastrar Usuário":
     cadastrar_usuario()
 elif menu == "Listar Usuários":
     listar_usuarios()
+elif menu == "Realizar Emprestimos":
+    cadastrar_emprestimo()
+elif menu == "Mostrar Emprestimos":
+    listar_emprestimos()
+elif menu == "Devolucoes":
+    cadastrar_devolucao()
