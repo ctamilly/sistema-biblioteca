54ef66392094bbbfb2ead029c5072d3473c3413e
Tamilly <116323270+ctamilly@users.noreply.github.com>
2025-04-08 18:54:10 -0300

    Update biblio_app.py

--- biblio_app.py
+++ biblio_app.py
@@ -1,24 +1,37 @@
 import streamlit as st
 from livros import cadastrar_livro, listar_livros
 from usuarios import cadastrar_usuario, listar_usuarios
-from emprestimo import cadastrar_emprestimo, listar_emprestimos
-from devolucao import cadastrar_devolucao
+from emprestimos import cadastrar_emprestimo, listar_emprestimos
+from devolucoes import cadastrar_devolucao
+from relatorios import mostrar_estatisticas
 
+st.set_page_config(page_title="Sistema de Biblioteca", layout="wide")
 st.title("Sistema de Biblioteca")
 
-menu = st.sidebar.radio("Menu", ["Cadastrar Livro", "Listar Livros", "Cadastrar Usuário", "Listar Usuários", "Realizar Emprestimos", "Mostrar Emprestimos", "Devolucoes"])
+secao = st.sidebar.selectbox("📂 Seções", ["Cadastros", "Empréstimos e Devoluções", "Listas e Buscas", "Relatórios"])
 
-if menu == "Cadastrar Livro":
-    cadastrar_livro()
-elif menu == "Listar Livros":
-    listar_livros()
-elif menu == "Cadastrar Usuário":
-    cadastrar_usuario()
-elif menu == "Listar Usuários":
-    listar_usuarios()
-elif menu == "Realizar Emprestimos":
-    cadastrar_emprestimo()
-elif menu == "Mostrar Emprestimos":
-    listar_emprestimos()
-elif menu == "Devolucoes":
-    cadastrar_devolucao()
+if secao == "Cadastros":
+    submenu = st.sidebar.radio(" ", ["Cadastrar Usuário", "Cadastrar Livro"])
+    if submenu == "Cadastrar Usuário":
+        cadastrar_usuario()
+    elif submenu == "Cadastrar Livro":
+        cadastrar_livro()
+    
+elif secao == "Empréstimos e Devoluções":
+    submenu = st.sidebar.radio(" ", ["Realizar Empréstimo", "Registrar Devolução"])
+    if submenu == "Realizar Empréstimo":
+        cadastrar_emprestimo()
+    elif submenu == "Registrar Devolução":
+        cadastrar_devolucao()
+        
+elif secao == "Listas e Buscas":
+    submenu = st.sidebar.radio(" ", ["Listar Livros", "Listar Usuários", "Listar Empréstimos"])
+    if submenu == "Listar Livros":
+        listar_livros()
+    elif submenu == "Listar Usuários":
+        listar_usuarios()
+    elif submenu == "Listar Empréstimos":
+        listar_emprestimos()
+        
+elif secao == "Relatórios":
+    mostrar_estatisticas()
