411dd40d6a861dd444be96424f1173c4fe350a83
Tamilly <116323270+ctamilly@users.noreply.github.com>
2025-04-08 18:54:55 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -4,10 +4,9 @@ import csv
 arquivos = {
     "livros.csv": ["Título", "Autor", "Ano", "ISBN", "Categoria"],
     "usuarios.csv": ["ID", "Nome", "Email", "Tipo", "Livros_Emprestados"],
-    "emprestimos.csv": ["Livro", "Usuário", "Data de Empréstimo"]
+    "emprestimos.csv": ["Livro", "Usuário", "Data de Emprestimo"]
 }
 
-# Cria arquivos com cabeçalhos, se ainda não existirem
 for nome_arquivo, cabecalhos in arquivos.items():
     caminho = os.path.join("data", nome_arquivo)
     if not os.path.exists(caminho):
@@ -29,7 +28,13 @@ def carregar_dados(nome_arquivo):
 
 def salvar_dados(nome_arquivo, dados, cabecalhos):
     caminho = os.path.join("data", nome_arquivo)
-    with open(caminho, "w", encoding="utf-8", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=cabecalhos)
-        writer.writeheader()
-        writer.writerows(dados)
+    try:
+        with open(caminho, "w", encoding="utf-8", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=cabecalhos)
+            writer.writeheader()
+            writer.writerows(dados)
+        return True
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo {nome_arquivo}: {e}")
+        return False
+
